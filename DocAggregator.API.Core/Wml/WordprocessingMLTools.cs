b6addf8d5103afa072bde2dc82688ffce27a972a
Анатолий Костин <AK_kostin@ntc.rosneft.ru>
2022-03-16 18:05:42 +0300

    Add style saving for content controls.

--- DocAggregator.API.Core/Wml/WordprocessingMLTools.cs
+++ DocAggregator.API.Core/Wml/WordprocessingMLTools.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace DocAggregator.API.Core.Wml
@@ -61,43 +59,74 @@ namespace DocAggregator.API.Core.Wml
                 XElement cell = innerContent.Element(W.tc);
                 if (cell != null)
                 {   // We are inside a table cell.
-                    Test(sdt, cell, cell.Element(W.p), insert);
+                    ReplaceContentControl(sdt, cell, insert);
                     continue;
                 }
                 XElement par = innerContent.Element(W.p);
                 if (par != null)
                 {   // We are in a paragraph.
-                    Test(sdt, par, par, insert);
+                    ReplaceContentControl(sdt, par, insert);
                     continue;
                 }
                 XElement run = innerContent.Element(W.r);
                 if (run != null)
                 {
-                    Test(sdt, run, run, insert);
+                    ReplaceContentControl(sdt, run, insert);
                     continue;
                 }
                 // TODO: Log, there are no text data.
             }
         }
 
-        public static void Test(XElement sdt, XElement innerContent, XElement textContainer, Insert insert)
+        /// <summary>
+        /// Put the value of <paramref name="insert"/> into <paramref name="innerTextContainer"/> using text properties of <paramref name="sdt"/>
+        /// then replace <paramref name="sdt"/> with <paramref name="innerTextContainer"/>.
+        /// </summary>
+        /// <param name="sdt">Content control.</param>
+        /// <param name="innerTextContainer">The inner element of <see cref="W.sdtPr"/> element.</param>
+        /// <param name="insert">Target value of the content control.</param>
+        public static void ReplaceContentControl(XElement sdt, XElement innerTextContainer, Insert insert)
         {
-            if (textContainer == null)
+            if (innerTextContainer == null)
             {
                 return;
             }
             switch (insert.Kind)
             {
                 case InsertKind.CheckMark:
-                    textContainer.Descendants(W.t).Single().Value = insert.ReplacedCheckmark.Value ? "☒" : "☐";
+                    innerTextContainer.Descendants(W.t).Single().Value = insert.ReplacedCheckmark.Value ? "☒" : "☐";
                     break;
                 default:
-                    textContainer.Descendants(W.rStyle).SingleOrDefault()?.Remove();
-                    textContainer.Descendants(W.t).Single().Value = insert.ReplacedText;
+                    innerTextContainer.Descendants(W.rStyle).SingleOrDefault()?.Remove();
+                    XElement tagRunPr = sdt.Element(W.sdtPr)?.Element(W.rPr);
+                    XElement run = innerTextContainer.DescendantsAndSelf(W.r).Single();
+                    if (tagRunPr != null && run != null)
+                    {
+                        XElement runPr = run.Element(W.rPr);
+                        if (runPr == null)
+                        {
+                            runPr = new XElement(W.rPr);
+                            run.Add(runPr);
+                        }
+                        foreach (XElement targetRunPr in tagRunPr.Elements())
+                        {
+                            XElement clone = new XElement(targetRunPr);
+                            XElement candidate = runPr.Element(targetRunPr.Name);
+                            if (candidate == null)
+                            {
+                                runPr.Add(clone);
+                            }
+                            else
+                            {
+                                candidate.ReplaceWith(clone);
+                            }
+                        }
+                    }
+                    innerTextContainer.Descendants(W.t).Single().Value = insert.ReplacedText;
                     break;
             }
-            sdt?.ReplaceWith(innerContent);
-            insert.AssociatedChunk = innerContent;
+            sdt?.ReplaceWith(innerTextContainer);
+            insert.AssociatedChunk = innerTextContainer;
         }
     }
 }
